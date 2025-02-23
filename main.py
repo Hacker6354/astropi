6974401cbcee6d7237aab1dadc08fb2be7a91d3b
Hacker6354 <goldmanmalkaor@gmail.com>
2025-02-23 16:14:11 +0100

    Update main.py
    even better version

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from time import sleep
 from pathlib import Path
 #from math import sin, cos, sqrt, atan2
 import math
+import cv2
+import os
 
 base_folder = Path(__file__).parent.resolve()
 
@@ -46,7 +48,6 @@ lon2 = dms_to_dd(lon[0],lon[1],lon[2],lon[3])
 
 # Calculate the distance 
 distance = find_distance(lat1, lon1, lat2, lon2)
-print("Resault", distance)
 
 print("Hello from the ISS")
 
@@ -60,9 +61,15 @@ with open(file_path, 'a', encoding="utf-8") as file:
     file.write(output_string)
 
 cam = Camera()
-cam.take_photo("image1.jpg")
-cam.take_photo("gps_image1.jpg", gps_coordinates=get_gps_coordinates(iss))
-cam.capture_sequence("sequence", num_images=40, interval=1)
+cloudimg = os.path.join("cloud.jpg")
+cam.take_photo(cloudimg)
+cam.capture_sequence("sequence", num_images=10, interval=3)
 
-
-print("Data written to", file_path)
+img = cv2.imread(cloudimg, 0)
+height = img.shape[0]
+width = img.shape[1]
+ret, thresh = cv2.threshold(img, 100, 1, cv2.THRESH_BINARY) 
+total = sum(map(sum, thresh)) # to find total sum of 2D array thresh
+percent = total/height/width*100
+percent_formatted = "{:.1f}".format(percent)
+print('percentage of cloud cover is =', percent_formatted, '%')
